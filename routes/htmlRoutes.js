b11e5426b0e955f66115c11d9af7eb1ddb87217a
Weslley Braga <wesbragagt@gmail.com>
2019-05-05 13:21:32 -0500

    files organized and server working

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,7 +5,8 @@ var path = require("path");
 module.exports = function(app) {
     // Load index page
     app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/dashboard.html"));
+        res.sendFile(path.join(__dirname, "../public/html/dashboard.html"));
+
         // db.Anime.findAll({}).then(function(dbExamples) {
         //     res.render("index", {
         //         msg: "AnimeGo!",
