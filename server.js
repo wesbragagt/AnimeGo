3a3bd3f2d29f4d876adbb8ab18f2c9b9c43659e6
KelseyGibbs <gibbs.kelseylee@gmail.com>
2019-05-10 00:43:27 -0500

    try 2938479278768

--- server.js
+++ server.js
@@ -21,9 +21,12 @@ app.engine(
     })
 );
 app.set("view engine", "handlebars");
+
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+require("dotenv").config();
+
 
 var syncOptions = { force: true };
 
