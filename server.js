6568080dc54ec681f6abda96380494f335980a56
Weslley Braga <wesbragagt@gmail.com>
2019-05-10 19:08:01 -0500

    last commit

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 require("dotenv").config();
 
-var syncOptions = { force: true };
+var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
